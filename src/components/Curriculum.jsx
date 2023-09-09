ea7685acb7222d24635f28e2d4e4263f097a6a7a
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2023-09-09 20:53:58 +0200

    Curriculum V2

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import '../css/Curriculum.css';
 import photo from '../img/photo.jpg';
+import giesecke from '../img/giesecke.jpg';
+import skills from '../img/skills.svg';
 
 export default class Curriculum extends Component{
     constructor(props){
@@ -23,14 +25,51 @@ export default class Curriculum extends Component{
                                 <p>
                                 I am a <strong>responsible</strong> person, eager to work, seeking to expand my <strong>work experience</strong> while continuing my studies as a <strong>telematic engineer</strong>. I have a <strong>high capacity</strong> for learning in any environment and I <strong>excel in computer</strong> and ICT environments.
                                 </p>
+								<div className="curriculum-header-contact"></div>
                             </div>
                         </div>
                     </section>
                     <section>
                         <div className="curriculum-body-top">
-                            <h2 className="curriculum-section-title">Personal Information</h2>
+                            <div className="curriculum-experience-titles">
+								<div className="curriculum-experience-title">
+									<h4>work experience</h4>
+								</div>
+								<div className="curriculum-experience-title">
+									<h4>education</h4>
+								</div>
+								<div className="curriculum-experience-title">
+									<h4>languages</h4>
+								</div>
+								<div className="curriculum-experience-title">
+									<h4>Courses</h4>
+								</div>
+							</div>
+							<div className="curriculum-experience-contents">
+								<div className="curriculum-experience-content">
+									<img className="curriculum-experience-content-image" src={giesecke} alt="image" />
+									<div className="curriculum-experience-content-info">
+										<div style={{display: "flex", alignItems: "center"}}>
+											<span className="curriculum-experience-content-title">Web Developer</span>
+											<span className="curriculum-experience-content-years">- &nbsp;2023 - actual</span>
+										</div>
+										<p className="curriculum-experience-content-description">
+											Frontend and backend development of web applications using HTML, CSS, JavaScript, PHP, MySQL, React, NodeJS, among others.
+										</p>
+									</div>
+									<div className="curriculum-experience-content-skills-container">
+										<div className="curriculum-experience-content-skills">
+											<img className="curriculum-skills-button" src={skills} alt="skills" />
+										</div>
+									</div>
+								</div>
+							</div>
                         </div>
                     </section>
+					<section>
+						<div className="curriculum-body-bottom">
+						</div>	
+					</section>
                 </div>
             </div>
         )
